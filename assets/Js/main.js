04e85146928a368f11f0eb010f5ab0e8648d8350
Didier Dakoury <dazibao76@gmail.com>
2021-09-17 17:50:06 +0000

    third commit

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,24 +1,17 @@
-<script>
-        $(".carousel").owlCarousel({
-            margin: 10,
-            loop: true,
-            autoplay: true,
-            autoplayTimeout: 2000, //2000 ms= 2sec 
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                    items: 1,
-                    nav: false,
-                },
-                600: {
-                    items: 2,
-                    nav: false,
-                },
-                1000: {
-                    items: 3,
-                    nav: false,
-                },
-                
-            }
+        
+        
+        $(document).ready(function(){
+            $(".owl-carousel").owlCarousel({
+                margin: 10,
+                loop: true,
+                nav: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                    },
+                    768: {
+                        items: 2,
+                    },
+                }
+            });
         });
-    </script>
