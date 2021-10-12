8ae117d31d29fffd6b7acfa3750fc57264d84aa8
Didier Dakoury <dazibao76@gmail.com>
2021-10-12 15:37:24 +0000

    second commit of October 8th

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -17,15 +17,16 @@
         });
 
 
-        let swiper = new Swiper('.swiper-container', {
+        let swiper = new Swiper('.portfolio__container', {
             cssMode: true,
             loop: true,
+
             navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
             },
             pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
+                el: '.swiper-pagination',
+                clickable: true,
             },
         });
