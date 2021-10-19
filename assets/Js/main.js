b19c6cbf77b2ad9453601dbc6fe8623be1d8d689
Didier Dakoury <dazibao76@gmail.com>
2021-10-19 19:04:56 +0000

    another change

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,20 +1,3 @@
-        
-        
-        // $(document).ready(function(){
-        //     $(".owl-carousel").owlCarousel({
-        //         margin: 10,
-        //         loop: true,
-        //         nav: true,
-        //         responsive: {
-        //             0: {
-        //                 items: 1,
-        //             },
-        //             600: {
-        //                 items: 2,
-        //             },
-        //         }
-        //     });
-        // });
 
 
         let swiperPortfolio = new Swiper('.portfolio__container', {
